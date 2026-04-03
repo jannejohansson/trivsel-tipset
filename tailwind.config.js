4331a8018bc10acdd2a32fe55cbf13dbf67a2ad9
Jan Johansson <jan.johansson@anticimexforsakringar.se>
2026-04-03 14:38:13 +0200

    Build Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-container': 'rgb(6 95 24 / 1)',
+        'on-primary-container': 'rgb(134 216 129 / 1)',
+        'surface': '#131313',
+        'surface-container-low': '#1c1b1b',
+        'surface-container-lowest': '#0e0e0e',
+        'surface-tone': '#0f1f42',
+        'text': '#e5e2e1',
+        'secondary': '#e9c349',
+        'primary': '#88d982'
+      },
+    },
+  },
+  plugins: [],
+};
